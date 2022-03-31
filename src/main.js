f614537026525cf3f06059671adc5aed14c6815e
lishujing <super_lishujing@163.com>
2022-03-31 15:22:54 +0800

    refactor(初始化): 项目代码初始化

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/**
+ * 1、通过按需导入的createApp方法来构建vue实例
+ * 2、通过vue实例.use方法来挂载插件(router、vuex)
+ * 3、没有了 Vue 构造方法，无法再挂载原型
+ *
+*/
 createApp(App).use(store).use(router).mount('#app')
