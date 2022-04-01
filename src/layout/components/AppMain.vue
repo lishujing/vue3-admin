a7f73cb5904f9e57045c9021db6ffd4dd1c2b5fa
lishujing <super_lishujing@163.com>
2022-04-01 17:31:39 +0800

    feat(layout): 新建首页布局框架

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>appmains</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/layout/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>navbar</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
