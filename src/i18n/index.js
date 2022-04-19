0c6adba168dd858ebe03b20293de2541792944fb
lishujing <super_lishujing@163.com>
2022-04-19 13:42:09 +0800

    feat(i18n): 处里 sidebar 和 面包屑的国际化 、 处理国际化缓存问题

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import mZhLocale from './lang/zh'
 import mEnLocale from './lang/en'
+import store from '@/store'
 const messages = {
   en: {
     msg: {
@@ -14,14 +15,18 @@ const messages = {
   }
 }
 
-const locale = 'en'
+// const locale = 'en'
+
+function getLanguage() {
+  return store && store.getters && store.getters.language
+}
 
 const i18n = createI18n({
   // 使用 Composition API 模式，则需要将其设置为 false
   legacy: false,
   // 全局注入 $t 函数
   globalInjection: true,
-  locale,
+  locale: getLanguage(),
   messages
 })
 
